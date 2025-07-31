82cb9d3a2188e9a98f9652df4647c39806b1fef7
mininxd <mininsna@gmail.com>
2025-07-31 12:09:56 +0000

    revert theme

--- patches/src/main/kotlin/app/revanced/patches/youtube/layout/theme/ThemePatch.kt
+++ patches/src/main/kotlin/app/revanced/patches/youtube/layout/theme/ThemePatch.kt
@@ -49,7 +49,7 @@ val themePatch = resourcePatch(
     )
 val darkThemeBackgroundColor = stringOption(
         key = "darkThemeBackgroundColor",
-        default = "#FF212121",
+        default = amoledBlackColor,
         values = availableDarkTheme,
         title = "Dark theme background color",
         description = "Can be a hex color (#AARRGGBB) or a color resource reference.",
